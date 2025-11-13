f1c27a285137c2ee788a23687ff5880e90218a72
AlexDoblyno <alexdoblyno@gmail.com>
2025-11-12 20:46:57 -0700

    Delete Play.css and html copied play folder and rest of it to src

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './src/login/login';
+import { Login } from './login/login';
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
-import { About } from './src/about/about';
-import { AuthState } from './src/login/authState';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
